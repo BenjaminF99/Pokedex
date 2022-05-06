da19f0407ba799e80abe0ead61f260c021147b9b
BenjaminF99 <benjamin1.kodehode@gmail.com>
2022-05-06 10:34:58 +0200

    Pokedex improved

--- pokedex.js
+++ pokedex.js
@@ -67,8 +67,8 @@ const search_btn = document.getElementById('btn')
 
 document.getElementById("search")
 .addEventListener("keyup", function(event) {
-event.preventDefault();
-if (event.keyCode === 13) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
     document.getElementById("btn").click();
 }
 });
